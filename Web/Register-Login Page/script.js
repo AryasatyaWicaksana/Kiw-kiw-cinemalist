cd67f0f1d81f17a0efc0efc99a846387df21afa7
SimonHutapea <simonm.p.hutapea@gmail.com>
2024-11-06 16:24:28 +0700

    Memperbaiki sedikit page login-register

--- Web/Register-Login Page/script.js
+++ Web/Register-Login Page/script.js
@@ -5,11 +5,11 @@ document.getElementById('togglePassword').addEventListener('click', function () 
     // Toggle the type attribute
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
-        passwordIcon.classList.remove('fa-eye');
-        passwordIcon.classList.add('fa-eye-slash');
+        passwordIcon.classList.remove('bi-eye-fill');
+        passwordIcon.classList.add('bi-eye-slash-fill');
     } else {
         passwordInput.type = 'password';
-        passwordIcon.classList.remove('fa-eye-slash');
-        passwordIcon.classList.add('fa-eye');
+        passwordIcon.classList.remove('bi-eye-slash-fill');
+        passwordIcon.classList.add('bi-eye-fill');
     }
 });
